5eade6f41f7945a97a3004478bfba33cf8dfb206
rohithy <67862635+Rohithyathiraj@users.noreply.github.com>
2023-06-02 14:44:13 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import BlogCreationPage from './BlogForm';
+function App() {
+  return (
+    <div className="App">
+      <BlogCreationPage/>
+      
+    </div>
+  );
+}
+
+export default App;
